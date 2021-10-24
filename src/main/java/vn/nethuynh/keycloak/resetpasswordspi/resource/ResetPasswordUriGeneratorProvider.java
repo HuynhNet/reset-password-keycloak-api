acd1ea78a70e64274c09f74c71fe4af118ecaa98
HuynhNet <net.huynh@axonactive.com>
2021-10-24 19:00:26 +0700

    Define reset password URI provider

---
+++ src/main/java/vn/nethuynh/keycloak/resetpasswordspi/resource/ResetPasswordUriGeneratorProvider.java
@@ -0,0 +1,16 @@
+package vn.nethuynh.keycloak.resetpasswordspi.resource;
+
+import org.keycloak.services.resource.RealmResourceProvider;
+
+public class ResetPasswordUriGeneratorProvider implements RealmResourceProvider{
+
+    @Override
+    public void close() {
+    }
+
+    @Override
+    public Object getResource() {
+        return new Object();
+    }
+    
+}
